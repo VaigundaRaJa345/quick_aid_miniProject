e3f42139645d2f998f8fbceed81b34b12bbc9749
vaigundaraja <vaigundaraja1313@gmail.com>
2025-03-14 10:51:56 +0530

    url

--- quick_aid/config.py
+++ quick_aid/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'your_secret_key'  # Change this to a strong random key
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  # Update as per your database
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # Update as per your database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
